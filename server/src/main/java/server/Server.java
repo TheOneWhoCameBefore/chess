3a314377203ca7dc8115deede6b9bfa7de6db900
TheOneWhoCameBefore <joshua.svenson9940@gmail.com>
2025-03-03 17:51:18 -0700

    Added services and clear endpoint to server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,14 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.GameDAO;
 import spark.*;
+import service.*;
 
 public class Server {
+    private final DatabaseService databaseService = new DatabaseService();
+    private final GameService gameService = new GameService();
+    private final UserService userService = new UserService();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,6 +16,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::clear);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
@@ -21,5 +28,11 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object clear(Request req, Response res) {
+        databaseService.clear();
+        res.status(200);
+        return "";
     }
 }
