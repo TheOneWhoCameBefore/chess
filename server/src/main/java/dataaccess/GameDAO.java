4210eddffd7bb01d2703d790dcbefde43fa7b2e5
Joshua Svenson <joshsven@byu.edu>
2025-03-12 15:46:29 -0600

    Modified updateGame to allow for changing the game name and object

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -9,7 +9,7 @@ public interface GameDAO {
     GameData createGame(String whiteUsername, String blackUsername, String gameName, ChessGame gameObject) throws DataAccessException;
     GameData retrieveGame(int gameID) throws DataAccessException;
     Collection<GameData> retrieveAllGames() throws DataAccessException;
-    GameData updateGame(int gameID, String whiteUsername, String blackUsername) throws DataAccessException;
+    GameData updateGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame gameObject) throws DataAccessException;
 //    void deleteGame(int gameID) throws DataAccessException;
     void deleteAllGames() throws DataAccessException;
 }
