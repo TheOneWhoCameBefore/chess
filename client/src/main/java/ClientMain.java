98bc8452c8433c769c64f00c05010fdd2fd5e9fe
Joshua Svenson <joshsven@byu.edu>
2025-03-25 22:44:22 -0600

    Added client main class

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import ui.Repl;
 
 public class ClientMain {
+    public static void main(String[] args) {
+        var serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+        new Repl(serverUrl).run();
+    }
+
 }
