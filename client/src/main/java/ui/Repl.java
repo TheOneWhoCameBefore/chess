ead66dd566888ef1ccf251fdf36e4de80d69e7f2
Joshua Svenson <joshsven@byu.edu>
2025-03-25 22:44:44 -0600

    Added repl class

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,46 @@
 package ui;
 
+import javax.management.Notification;
+import java.util.Scanner;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.Constants.RESET;
+import static java.awt.Color.*;
+
 public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("♕ Welcome to 240 chess. Type Help to get started. ♕");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+//    public void notify(Notification notification) {
+//        System.out.println(RED + notification.message());
+//        printPrompt();
+//    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+
 }
