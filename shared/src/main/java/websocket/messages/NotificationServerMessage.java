248237e6492da761a9e891bf5216801c7b42c71b
Joshua Svenson <joshsven@byu.edu>
2025-04-08 00:46:16 -0600

    Created notifcation server message

--- shared/src/main/java/websocket/messages/NotificationServerMessage.java
+++ shared/src/main/java/websocket/messages/NotificationServerMessage.java
@@ -1,4 +1,17 @@
 package websocket.messages;
 
-public class NotificationServerMessage {
+public class NotificationServerMessage extends ServerMessage {
+    private final String message;
+
+    public NotificationServerMessage(ServerMessageType type, String message) {
+        super(type);
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
+
+    @Override
+    public String toString() {
+        return message;
+    }
 }
