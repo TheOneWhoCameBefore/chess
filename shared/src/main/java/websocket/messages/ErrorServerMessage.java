f620ed2c9dc037b0e40f6913309c9353751be82d
Joshua Svenson <joshsven@byu.edu>
2025-04-09 18:29:58 -0600

    Created error server message

--- shared/src/main/java/websocket/messages/ErrorServerMessage.java
+++ shared/src/main/java/websocket/messages/ErrorServerMessage.java
@@ -1,4 +1,15 @@
 package websocket.messages;
 
-public class ErrorServerMessage {
+import java.security.PrivateKey;
+import java.sql.PreparedStatement;
+
+public class ErrorServerMessage extends ServerMessage {
+    private final String message;
+
+    public ErrorServerMessage(ServerMessageType type, String message) {
+        super(type);
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
 }
